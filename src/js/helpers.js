d37b3afa00e57865d2ec019c1d843c96d20ce30f
Mrilyes <ilyesghlila1@gmail.com>
2022-07-27 16:58:32 +0100

    Listening for load and hashchange events

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,6 +1,6 @@
 // The goal of this module is to contain a couple of functions that se reuse over and over in the project
 import { async } from 'regenerator-runtime';
-import { TIMEOUT_SEC } from './helpers.js';
+import { TIMEOUT_SEC } from './config.js';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
