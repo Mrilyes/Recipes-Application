312c45ebfc3790ea1daaba97647ac3d8e02d907d
Mrilyes <ilyesghlila1@gmail.com>
2022-07-30 21:03:36 +0100

    Helpers and configuration files

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 // The goal is to allow us easy configuer the project by simply changing some of the data in config.js file
 // config.env for backend
 
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
